6388d388a05b48610924d730b03ad94436c0bb91
andres alcocer <andresalcocer7@yahoo.com>
2019-06-07 10:36:08 -0400

    only members of a channel can acess private channels

--- Slack-Clone-Server/resolvers/team.js
+++ Slack-Clone-Server/resolvers/team.js
@@ -69,7 +69,16 @@ export default {
     }),
   },
   Team: {
-    channels: ({ id }, args, { models }) => models.Channel.findAll({ where: { teamId: id } }),
+    channels: ({ id }, args, { models, user }) => models.sequelize.query(`
+    select distinct on(id) *
+    from channels as c, private_channel_members as pc where
+    c.team_id = :teamId and
+    (c.public = true or (pc.user_id = :userId and c.id = pc.channel_id));`,
+    {
+      replacements: { teamId: id, userId: user.id },
+      model: models.Channel,
+      raw: true,
+    }),
     directMessageMembers: ({ id }, args, { models, user }) => models
       .sequelize.query('select distinct on (u.id) u.id, u.username from users as u join direct_messages as dm on (u.id = dm.sender_id) or (u.id = dm.receiver_id) where (:currentUserId = dm.sender_id or :currentUserId = dm.receiver_id) and dm.team_id = :teamId', {
         replacements: { currentUserId: user.id, teamId: id },
